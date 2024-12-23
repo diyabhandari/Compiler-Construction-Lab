7ff5b7c28ce9377b420963ddb2fb232df5d83112
Diya Bhandari <diyabhandari@gmail.com>
2024-12-23 19:11:56 +0530

    add expt 1

---
+++ Experiment_1/experiment_1.cpp
@@ -0,0 +1,105 @@
+//wap to implement a DFA for a language that accepts strings with even number of a's and odd number of b's
+#include<iostream>
+#include <string>
+using namespace std;
+class DFA{
+  private: 
+    int dfa=0;
+    //each state has its own function to define the transition to next state on input
+    //depending on char received, we move to corresponding state of DFA
+    void start(char c){ 
+      switch(c){
+        case 'a':
+          dfa = 2;
+          break;
+        case 'b':
+          dfa = 1;
+          break;
+        default: //for anything other than a/b
+          dfa = -1;
+          break;
+
+      }
+    }
+    void state1(char c){ //this is also the final state
+      switch(c){
+        case 'a':
+          dfa = 3;
+          break;
+        case 'b':
+          dfa = 0;
+          break;
+        default:
+          dfa = -1;
+          break;
+      }
+    }
+    void state2(char c){
+      switch(c){
+        case 'a':
+          dfa = 0;
+          break;
+        case 'b':
+          dfa = 3;
+          break;
+        default:
+          dfa = -1;
+          break;
+      }
+    }
+    void state3(char c){
+      switch(c){
+        case 'a':
+          dfa = 1;
+          break;
+        case 'b':
+          dfa = 2;
+          break;
+        default:
+          dfa = -1;
+          break;
+      }
+    }
+    int isAccepted(string input){
+      int len = input.size();
+      for(int i=0;i<len;i++){
+        switch(dfa){
+          case 0:
+            start(input.at(i));
+            break;
+          case 1:
+            state1(input.at(i));
+            break;
+          case 2:
+            state2(input.at(i));
+            break;
+          case 3:
+            state3(input.at(i));
+            break;
+          default:
+            cout<<"state does not exist";
+            break;
+        }       
+      }
+      if(dfa==1)
+        return 1;
+      else 
+        return 0;
+    }
+  public:
+   void checkString(string input){
+    int result = isAccepted(input);
+    if(result==1)
+      cout<<"\nACCEPTED";
+    else
+      cout<<"\nNOT ACCEPTED";
+   }
+};
+int main(){
+  string inputString;
+  cout<<"Enter a string : ";
+  cin>>inputString;
+  DFA dfa;
+  dfa.checkString(inputString);
+  return 0;
+}
